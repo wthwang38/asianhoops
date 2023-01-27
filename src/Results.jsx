e191ff80d1a19317444c877dc9805ea929ee0238
William Hwang <willhwang90@gmail.com>
2023-01-27 15:21:11 -0500

    added Results component

---
+++ src/Results.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Results = () => {
+
+let currentYear = 2022;
+let searchYear;
+let divison;
+
+  return (
+    <div>
+      <header>{currentYear} Results</header>
+    
+    
+    </div>
+    
+  )
+}
