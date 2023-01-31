310261cbb5c6d33d5f733d1aec4c18a0e9097632
William Hwang <willhwang90@gmail.com>
2023-01-31 17:07:39 -0500

    changed up card to take in props

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
-function Card(){
-    let scoreboard = <img src="Score-Group-A.jpg"/>;
-    let scoreboard2 = <img src="Score-group-B.jpg"/>
+function Card({teamName, teamWin, teamLoss}){
+    let scoreboard = <img src="Score-Group-A.jpg" />;
+
     return(
         <div>
-        <div class="overflow-hidden bg-white py-24 sm:py-32">
-            <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                <div class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                        <div class="mt-6 text-lg leading-8 text-gray-600">
-                            {scoreboard}
-                        </div>
-                    </div>                            
-                </div>  
-            </div> 
-         </div>        
-    
+            <table className="m-auto">
+                <thead>
+                    <tr className="flex m-auto">
+                        <td className="border-px border-solid text-center px-5 w-1/5">{teamName}</td>
+                        <td className="border-px border-solid text-center px-5 w-1/5">{teamWin}</td>
+                        <td className="border-px border-solid text-center px-5 w-1/5">{teamLoss}</td>
+                    </tr>
+                </thead>
+            </table>
+         </div>       
     )
 }
 
--- src/Results.jsx
+++ src/Results.jsx
@@ -6,7 +6,36 @@ export const Results = () => {
 let currentYear = 2022;
 let searchYear;
 let divison;
-
+let scoreboard2 = [
+  {
+    "id": 1,
+    "name": "RAGING DEMONZ",
+    "won": 2,
+    "loss": 3
+  },
+  {
+    "id": 2,
+    "name": "NY FLIGHTZ",
+    "won": 2,
+    "loss": 3
+  },
+  {
+    "id": 3,
+    "name": "COLUMBUS PARK",
+    "won": 1,
+    "loss": 4
+  },
+  {
+    "id": 4,
+    "name": "ROCKITS ALUMNI",
+    "won": 4,
+    "loss": 1
+  }
+]
+  
+  const teamData = scoreboard2.map((team)=>{
+    return (<Card key={team.id} teamName={team.name} teamWin={team.won} teamLoss={team.loss} />)
+  })
   return (
     <div>
       <header className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{currentYear} Results</header>
@@ -26,12 +55,18 @@ let divison;
           <div>x</div>
         </div>
           <div className="lg:pl-[19.5rem] ml-20">
-            <Card />
+            <div className="flex flex-row">
+                  <div className="basis-1/2 border-solid border-2">Name</div>
+                  <div className="basis-1/2 border-solid border-2">Won</div >
+                  <div className="basis-1/2 border-solid border-2">Loss</div>
+            </div>
+              <div className="">{teamData}</div>
+            </div>
+
           </div>
       </div>
 
       </div>
-    </div>
 
     
   )
