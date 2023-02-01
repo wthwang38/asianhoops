30ed9acfb9bb43f31e6db9d1304e480b0e475331
JamesNg <njames15@gmail.com>
2023-02-01 15:43:25 -0500

    updated navBar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Routes, Route, useNavigate } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div>
-
-      <nav className="flex items-center justify-between flex-wrap bg-green-300 p-6">
-        <img className='object-cover h-48 w-100' src='src/assets/basketball.png'/>
-        
-        <div className="flex items-center flex-shrink-0 text-red mr-6">
-          <span className="font-bold text-lg">Asian Hoops</span>
+      <nav className="flex items-center justify-between flex-wrap bg-slate-100 p-6">
+        <div className="flex items-center flex-shrink-0 text-red ml-10 mr-10">
+          <span className="font-bold text-5xl">Asian Hoops</span>
         </div>
+        
+        <img className='pl-10 object-cover items-center h-48 w-100' src='src/assets/basketball.png'/>        
 
         <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
           <div className="text-sm sm:flex-grow">
@@ -21,15 +19,14 @@ function Navbar() {
               ['Blogs', '/blogs'],
               ['Results', '/results'],
             ].map(([title, url]) => (
-              <Link className='block mt-4 sm:inline-block sm:mt-0 text-blue-600 hover:text-white mr-4' to={url}>{title}</Link>
+              <Link className='block mt-4 ml-4 mr-4 sm:inline-block sm:mt-0 text-xl text-red-600 hover:text-blue' to={url}>{title}</Link>
               ))}
           </div>
-
-          <div>
-            <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-blue border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0">
+          {/* <div>
+            <button className="inline-block text-md px-4 py-2 leading-none border rounded text-blue border-black hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0">
               Login
             </button>
-          </div>
+          </div> */}
         </div>
       </nav>
     </div>
