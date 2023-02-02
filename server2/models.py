5483c03a8a61e41d486ae857e8fc5db2f824678c
Michelle <clairsin@gmail.com>
2023-02-02 15:54:27 -0500

    corrected models.py foreign key relationships

--- server2/models.py
+++ server2/models.py
@@ -25,27 +25,27 @@ class League(db.Model):
         return "<League: {self.name}>".format(self=self)
 
 class Team(db.Model):
-    # __tablename__ = 'teams'
+    __tablename__ = 'teams'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     wins = db.Column(db.Integer, default=0)
     losses = db.Column(db.Integer, default=0)
-    # league = db.Column(db.Integer, db.ForeignKey('league.id'), nullable=False)
+    league = db.Column(db.Integer, db.ForeignKey('leagues.id'), nullable=False)
 
-    def __init__(self, name, wins, losses):
+    def __init__(self, name, wins, losses, league):
         self.name = name
         self.wins = wins
         self.losses = losses
-        # self.league = league
+        self.league = league
 
     def __repr__(self):
         return "<Team: {self.name} has {self.wins} W and {self.losses} L>".format(self=self)
 
 class Game(db.Model):
-# __tablename__ = 'games'
+    __tablename__ = 'games'
     id = db.Column(db.Integer, primary_key=True)
-    home_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
-    away_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    away_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     home_score = db.Column(db.Integer)
     away_score = db.Column(db.Integer)
 
