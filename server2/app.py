0d493eaa387319f72147dace6ac9a93dd8891c79
Michelle <clairsin@gmail.com>
2023-02-02 14:21:37 -0500

    updated app.py, models.py, seeded example data

--- server2/app.py
+++ server2/app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+import os
+from flask import Flask, send_file, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from config import Config
+from models import db, Game, Team, League
+import json
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public')
+CORS(app, origins=['*'])
+app.config.from_object(Config)
+jwt = JWTManager(app)
+db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.get('/')
+def home():
+    return send_file('welcome.html')
+
+@app.route('/leagues')
+def get_all_leagues():
+    leagues = League.query.all()
+    if len(leagues):
+        for league in leagues:
+            return jsonify([league.toJSON() for league in leagues])
+    else:
+        return {}, 404
+
+
+# @app.get('/leagues')
+# def get_all_leagues():
+#     leagues = League.query.all()
+#     if len(leagues):
+#         for league in leagues:
+#             return jsonify([league.toJSON() for league in leagues])
+#     else:
+#         return {}, 404
+
 
 @app.route("/hi")
 def hello():
-    return "<p>Hi!</p>"
+    return "<p>Hiiii!</p>"
+
+
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=os.environ.get('PORT', 3001))
--- server2/seeds.py
+++ server2/seeds.py
@@ -1,2 +1,68 @@
 from app import app
-# from models import
+from models import db, Game, Team, League
+
+
+def run_seeds():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        print('Seeding database ... 🌱')
+        # league = [
+        #   ['amateur','this is a test'], 
+        league1 = League("amateur")
+        league2 = League("pro")
+        #   ["autl","definitely a test"]
+        # ]
+        # team = Team("will", 0, 0, 1)
+        # db.session.add(team)
+    
+    # for league in leagues:
+    #   new_league = League(league)
+
+        db.session.add(league1)
+        db.session.add(league2)
+        db.session.commit()
+
+        # rooms = ['Turing', 'Collins', 'Kay', 'Borg', 'Johnson']
+        # event_names = ['Rails', 'Python', 'React']
+        # start_times = ['01/13/23 10:00:00',
+        #                '01/13/23 13:00:00', '01/13/23 17:00:00']
+        # end_times = ['01/13/23 11:00:00',
+        #              '01/13/23 14:00:00', '01/13/23 18:00:00']
+
+        # for room in rooms:
+        #     new_classroom = Classroom(room)
+        #     db.session.add(new_classroom)
+        #     db.session.commit()
+        #     for i in range(3):
+        #         new_event = Event(event_names[i], datetime.strptime(
+        #             start_times[i], '%m/%d/%y %H:%M:%S'), datetime.strptime(end_times[i], '%m/%d/%y %H:%M:%S'), new_classroom.id)
+        #         db.session.add(new_event)
+        #         db.session.commit()
+        #         for i in range(1, 17):
+        #             new_seat = Seat(new_event.id, i, True, '')
+        #             db.session.add(new_seat)
+        #             db.session.commit()
+        #     print(new_classroom.toJSON())
+        # new_booking_1 = Booking(user1.id, 10)
+        # seat1 = Seat.query.get(10)
+        # seat1.is_empty = False
+        # seat1.student_name = user1.username
+        # db.session.add(new_booking_1)
+        # db.session.commit()
+        # new_booking_1 = Booking(user1.id, 20)
+        # seat2 = Seat.query.get(20)
+        # seat2.is_empty = False
+        # seat2.student_name = user1.username
+        # db.session.add(new_booking_1)
+        # db.session.commit()
+        # new_booking_1 = Booking(user1.id, 30)
+        # seat3 = Seat.query.get(30)
+        # seat3.is_empty = False
+        # seat3.student_name = user1.username
+        # db.session.add(new_booking_1)
+        # db.session.commit()
+    print('Done! 🌳')
+
+
+run_seeds()
